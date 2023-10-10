f3dde6afcb54b3a030a0581161372135f34e66bf
Andreas Sünder <asuender@student.tgm.ac.at>
2023-10-10 12:49:41 +0200

    Add project data file

---
+++ src/data.js
@@ -0,0 +1,81 @@
+export const team = [
+  { 
+    name: 'Andreas Sünder', role: 'Data Scientist & Project Lead',
+    image: { src: '~/assets/images/team/asuender.png', alt: 'Andreas Sünder' },
+  },
+  { 
+    name: 'Samira Ghaffari', role: 'Reseacher',
+    image: { src: '~/assets/images/team/sghaffari.png', alt: 'Samira Ghaffari' },
+  },
+  {
+    name: 'Daniel Lengsteiner', role: 'Data Scientist',
+    image: { src: '~/assets/images/team/dlengsteiner.png', alt: 'Daniel Lengsteiner' },
+  },
+  { 
+    name: 'Nicolas Pfeiler', role: 'Frontend Developer' ,
+    image: { src: '~/assets/images/team/npfeiler.png', alt: 'Nicolas Pfeiler' },
+  },
+];
+
+export const features = [
+  {
+    title: 'Feature 1',
+    description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
+    icon: 'flat-color-icons:document',
+  },
+  {
+    title: 'Feature 2',
+    description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
+    icon: 'flat-color-icons:template',
+  },
+  {
+    title: 'Feature 3',
+    description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
+    icon: 'flat-color-icons:voice-presentation',
+  },
+];
+
+export const milestones = [
+  {
+    title: 'Exploration',
+    description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+    deadline: new Date('2023-10-31'),
+    active: true,
+    icon: 'tabler:search',
+  },
+  {
+    title: 'Trained machine learning models',
+    description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+    deadline: new Date('2023-11-30'),
+    active: false,
+    icon: 'tabler:robot',
+  },
+  {
+    title: 'Deployed user interface & visualizations ',
+    description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+    deadline: new Date('2024-01-10'),
+    active: false,
+    icon: 'tabler:app-window',
+  },
+  {
+    title: "Researched legal situation",
+    description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+    deadline: new Date('2024-01-15'),
+    active: false,
+    icon: 'tabler:gavel',
+  },
+  {
+    title: "Integrated topic labelling",
+    description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+    deadline: new Date('2024-01-17'),
+    active: false,
+    icon: 'tabler:tags',
+  },
+  {
+    title: "Finished project",
+    description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+    deadline: new Date('2024-04-02'),
+    active: false,
+    icon: 'tabler:pennant',
+  },
+];
