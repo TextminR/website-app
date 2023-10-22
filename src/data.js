602acc2652e980f5a8f794a28f2f29a410f7a090
Andreas Sünder <asuender@student.tgm.ac.at>
2023-10-22 23:08:19 +0200

    Finish proposal for index page

--- src/data.js
+++ src/data.js
@@ -1,79 +1,83 @@
 export const team = [
   { 
-    name: 'Andreas Sünder', role: 'Data Scientist & Project Lead',
+    name: 'Andreas Sünder', role: 'Project Lead & AI Expert',
+    email: 'asuender@student.tgm.ac.at',
     image: { src: '~/assets/images/team/asuender.png', alt: 'Andreas Sünder' },
   },
   { 
     name: 'Samira Ghaffari', role: 'Reseacher',
+    email: 'sghaffari@student.tgm.ac.at',
     image: { src: '~/assets/images/team/sghaffari.png', alt: 'Samira Ghaffari' },
   },
   {
     name: 'Daniel Lengsteiner', role: 'Data Scientist',
+    email: 'dlengsteiner@student.tgm.ac.at',
     image: { src: '~/assets/images/team/dlengsteiner.png', alt: 'Daniel Lengsteiner' },
   },
   { 
-    name: 'Nicolas Pfeiler', role: 'Frontend Developer' ,
+    name: 'Nicolas Pfeiler', role: 'Frontend Developer',
+    email: 'npfeiler@student.tgm.ac.at',
     image: { src: '~/assets/images/team/npfeiler.png', alt: 'Nicolas Pfeiler' },
   },
 ];
 
 export const features = [
   {
-    title: 'Feature 1',
-    description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
-    icon: 'flat-color-icons:document',
+    title: 'Explore Trends',
+    description: 'Gain insights into how the occurrence of topics changes over different time periods and explore trends in your data.',
+    icon: 'flat-color-icons:bullish',
   },
   {
-    title: 'Feature 2',
-    description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
-    icon: 'flat-color-icons:template',
+    title: 'Real time analysis',
+    description: 'Analyze large volumes of text data within seconds and access the visualized results without delay.',
+    icon: 'flat-color-icons:clock',
   },
   {
-    title: 'Feature 3',
-    description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
-    icon: 'flat-color-icons:voice-presentation',
+    title: 'Use in education',
+    description: 'With our user-friendly interface, you can easily use our tool in education and apply it for real-world data.',
+    icon: 'flat-color-icons:graduation-cap',
   },
 ];
 
 export const milestones = [
   {
     title: 'Exploration',
-    description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+    description: "We will dive into the topic of text analysis and explore the various methods to achieve it.",
     deadline: new Date('2023-10-31'),
     active: true,
     icon: 'tabler:search',
   },
   {
     title: 'Trained machine learning models',
-    description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+    description: "By this stage, we will have trained at least one machine learning model to be used for topic extraction.",
     deadline: new Date('2023-11-30'),
     active: false,
     icon: 'tabler:robot',
   },
   {
-    title: 'Deployed user interface & visualizations ',
-    description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+    title: 'Deployed user interface & visualizations',
+    description: "We will have deployed a minimal user interface as a web app to visualize the results of the topic extraction.",
     deadline: new Date('2024-01-10'),
     active: false,
     icon: 'tabler:app-window',
   },
   {
     title: "Researched legal situation",
-    description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+    description: "We will have researched the legal situation regarding the use of the tool and the output it produces.",
     deadline: new Date('2024-01-15'),
     active: false,
     icon: 'tabler:gavel',
   },
   {
     title: "Integrated topic labelling",
-    description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+    description: "We will have integrated a production-ready language model to label the extracted topics.",
     deadline: new Date('2024-01-17'),
     active: false,
     icon: 'tabler:tags',
   },
   {
     title: "Finished project",
-    description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+    description: "We will have finished the project and will be ready to present it to the public.",
     deadline: new Date('2024-04-02'),
     active: false,
     icon: 'tabler:pennant',
